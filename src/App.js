6a786815cc6e7f4ed562ea4f386652497a6fad20
Jan Wojcik <123651940+Janowoj@users.noreply.github.com>
2023-08-12 06:50:28 +0200

    "added functions"

--- src/App.js
+++ src/App.js
@@ -1,28 +1,46 @@
 import './App.css';
+import React from 'react';
 
 function App() {
+  const [input, setInput] = React.useState('');
+  const [output, setOutput] = React.useState(0);
+
+  const display = (symbol) => {
+    setInput(prev => prev + symbol);
+  };
+
+  const calculate = () => {
+    setOutput(eval(input));
+  }
+
+  const allClear = () => {
+    setInput('');
+    setOutput(0);
+  }
+
   return (
     <div className="App">
       <div id='calculator'>
-        <input id='display'></input>
+        <input type='text' id='display' value={input} placeholder="0" disabled></input>
+        <div id='total'>{output}</div>
         <div id='buttons'>
-          <div id='clear'>AC</div>
-          <div id='divide'>/</div>
-          <div id='multiply'>X</div>
-          <div id='seven'>7</div>
-          <div id='eight'>8</div>
-          <div id='nine'>9</div>
-          <div id='substract'>-</div>
-          <div id='four'>4</div>
-          <div id='five'>5</div>
-          <div id='six'>6</div>
-          <div id='add'>+</div>
-          <div id='one'>1</div>
-          <div id='two'>2</div>
-          <div id='three'>3</div>
-          <div id='zero'>0</div>
-          <div id='decimal'>.</div>
-          <div id='equals'>=</div>
+          <div onClick={allClear} className="button" id='clear'>AC</div>
+          <div onClick={() => display("/")} className="button" id='divide'>/</div>
+          <div onClick={() => display("*")} className="button" id='multiply'>x</div>
+          <div onClick={() => display("7")} className="button" id='seven'>7</div>
+          <div onClick={() => display("8")} className="button" id='eight'>8</div>
+          <div onClick={() => display("9")} className="button" id='nine'>9</div>
+          <div onClick={() => display("-")} className="button" id='subtract'>-</div>
+          <div onClick={() => display("4")} className="button" id='four'>4</div>
+          <div onClick={() => display("5")} className="button" id='five'>5</div>
+          <div onClick={() => display("6")} className="button" id='six'>6</div>
+          <div onClick={() => display("+")} className="button" id='add'>+</div>
+          <div onClick={() => display("1")} className="button" id='one'>1</div>
+          <div onClick={() => display("2")} className="button" id='two'>2</div>
+          <div onClick={() => display("3")} className="button" id='three'>3</div>
+          <div onClick={() => display("0")} className="button" id='zero'>0</div>
+          <div onClick={() => display(".")} className="button" id='decimal'>.</div>
+          <div onClick={calculate} className="button" id='equals'>=</div>
         </div>
       </div>
     </div>
